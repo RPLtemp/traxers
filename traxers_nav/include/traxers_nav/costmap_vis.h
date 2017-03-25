6286570bb44247763de5a2c4bf5432ab42a36388
pvechersky <pvechersky@gmail.com>
2017-03-25 20:53:05 +1000

    Making costmap visualization work with both OpenCV 2 and 3

--- traxers_nav/include/traxers_nav/costmap_vis.h
+++ traxers_nav/include/traxers_nav/costmap_vis.h
@@ -1,9 +1,14 @@
 #ifndef TRAXERS_COSTMAP_VIS_H
 #define TRAXERS_COSTMAP_VIS_H
 
-#include <opencv2/contrib/contrib.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+#if CV_MAJOR_VERSION == 3
+  #include <opencv2/imgproc/imgproc.hpp>
+#elif CV_MAJOR_VERSION == 2
+  #include <opencv2/contrib/contrib.hpp>
+#endif
 
 namespace traxers {
 
